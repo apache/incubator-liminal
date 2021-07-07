ab7350c6ad840c7b566fe7d3654658c5c21bceb5
Simon Levin <simon@naturalint.com>
2021-07-07 21:41:07 +0300

    Update serving.py

--- examples/aws-ml-app-demo/serving.py
+++ examples/aws-ml-app-demo/serving.py
@@ -1,5 +1,7 @@
 import json
 
+from flask import jsonify 
+
 import model_store
 from model_store import ModelStore
 
@@ -10,9 +12,21 @@ def predict(input_json):
     print(f'input_json={input_json}')
     input_dict = json.loads(input_json)
     model, version = _MODEL_STORE.load_latest_model()
+    if version is None:
+        return jsonify(
+            message="Model is not ready yet",
+            category="fail",
+            status=404
+        )
+
     result = str(model.predict_proba([[float(input_dict[_PETAL_WIDTH])]])[0][1])
     print(f'result={result}')
-    return result
+    return jsonify(
+        result=result,
+        category="success",
+        version=f"{version}",
+        status=200
+    )
 
 def healthcheck(self):
     return 'Server is up!'
