52f8250239d92329e81312a3338e002aad0f5e74
Simon Levin <simon@naturalint.com>
2021-07-08 12:05:07 +0300

    Update serving.py

--- examples/aws-ml-app-demo/serving.py
+++ examples/aws-ml-app-demo/serving.py
@@ -10,23 +10,19 @@ _PETAL_WIDTH = 'petal_width'
 
 def predict(input_json):
     print(f'input_json={input_json}')
-    input_dict = json.loads(input_json)
-    model, version = _MODEL_STORE.load_latest_model()
-    if version is None:
-        return jsonify(
-            message="Model is not ready yet",
-            category="fail",
-            status=404
-        )
+    try:
+        input_dict = json.loads(input_json)
+        model, version = _MODEL_STORE.load_latest_model()
+        result = str(model.predict_proba([[float(input_dict[_PETAL_WIDTH])]])[0][1])
+        print(f'result={result}')
+        return result
 
-    result = str(model.predict_proba([[float(input_dict[_PETAL_WIDTH])]])[0][1])
-    print(f'result={result}')
-    return jsonify(
-        result=result,
-        category="success",
-        version=f"{version}",
-        status=200
-    )
+    except IndexError:
+        return 'Failure: the model is not ready yet'
+
+    except Exception as e:
+        print(e)
+        return 'Failure'
 
 def healthcheck(self):
     return 'Server is up!'
