bc7c26bd7f6b66318ed96cbb97c6aeb7939184c4
Simon Levin <simon@naturalint.com>
2021-07-11 20:59:32 +0300

    Update serving.py

--- examples/aws-ml-app-demo/serving.py
+++ examples/aws-ml-app-demo/serving.py
@@ -1,18 +1,38 @@
 import json
 
+import flask
+
 import model_store
 from model_store import ModelStore
+from flask import jsonify
 
 _MODEL_STORE = ModelStore(model_store.PRODUCTION)
 _PETAL_WIDTH = 'petal_width'
 
+
 def predict(input_json):
-    print(f'input_json={input_json}')
-    input_dict = json.loads(input_json)
-    model, version = _MODEL_STORE.load_latest_model()
-    result = str(model.predict_proba([[float(input_dict[_PETAL_WIDTH])]])[0][1])
-    print(f'result={result}')
-    return result
+    try:
+        input_dict = json.loads(input_json)
+        model, version = _MODEL_STORE.load_latest_model()
+        result = str(model.predict_proba([[float(input_dict[_PETAL_WIDTH])]])[0][1])
+        return flask.jsonify({"result": result, "version": version})
+
+    except IndexError:
+        return 'Failure: the model is not ready yet'
+
+    except Exception as e:
+        print(e)
+        return 'Failure'
+
 
 def healthcheck(self):
     return 'Server is up!'
+
+
+def version(self):
+    try:
+        model, version = _MODEL_STORE.load_latest_model()
+        print(f'version={version}')
+        return version
+    except Exception as e:
+        return e
