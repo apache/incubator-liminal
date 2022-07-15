abdebc5e15e7609190918022d3fcd41183d9e3be
lidor ettinger <lidor.ettinger@gmail.com>
2022-07-16 01:12:08 +0300

    add license

--- examples/sagemaker_example/sm_ops.py
+++ examples/sagemaker_example/sm_ops.py
@@ -1,3 +1,20 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 
 import argparse
 import os
@@ -7,9 +24,6 @@ import boto3
 import numpy as np
 import pandas as pd
 import sagemaker
-#from sagemaker.deserializers import NumpyDeserializer
-#from sagemaker.predictor import Predictor
-#from sagemaker.serializers import NumpySerializer
 from sagemaker.sklearn.estimator import SKLearn
 from sagemaker.sklearn.model import SKLearnModel
 
@@ -22,8 +36,6 @@ from data_train.train import feature_column_names
 FRAMEWORK_VERSION = "0.23-1"
 
 sm_boto3 = boto3.client("sagemaker")
-
-sess = sagemaker.Session()
 
 
 def get_role():
